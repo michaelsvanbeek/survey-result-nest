999f0db4041119dfd83de0ca12437d8e2d0b557b
= <=>
2019-12-29 00:13:14 -0800

    resolve deprecation warnings

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,9 +1,15 @@
 import * as mongoose from 'mongoose';
 
+const mongooseOptions = {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://localhost/nest'),
+      mongoose.connect('mongodb://localhost/nest', mongooseOptions),
   },
 ];
